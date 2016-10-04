7b8b9ff44bcbbf80dcc99367780f14223479efea
watcher <paradoxkb@gmail.com>
2016-10-04 23:25:48 +0300

    added Registration Component

--- client/components/Content/Customers/Customers.js
+++ client/components/Content/Customers/Customers.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import Registration from './Registration'
+import './styles.css'
 
 export default class Customers extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Customers button to add user</h1>
-      </div>
-    )
-  }
+    addUserHandler (e) {
+        e.preventDefault()
+        let _user = {
+            name: e.target[0].value,
+            userName: e.target[1].value,
+            email: e.target[2].value,
+            birthDate: e.target[3].value,
+            phone: e.target[4].value,
+            password: e.target[5].value
+        }
+    }
+    render() {
+        return (
+            <div>
+                <h1>Customers</h1>
+                <input className='btn btn-default' id='add_user_button' name='make-action' defaultValue='Registration' />
+                <div id='add_user_form'>
+                    <h3>Register user</h3>
+                    <Registration register={this.addUserHandler} />
+                </div>
+            </div>
+        )
+    }
 }
