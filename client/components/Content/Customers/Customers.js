aac3ad0d5b3ddab4b7fab0b194e81993074f1308
watcher <paradoxkb@gmail.com>
2016-10-05 12:24:38 +0300

    current version

--- client/components/Content/Customers/Customers.js
+++ client/components/Content/Customers/Customers.js
@@ -1,29 +1,50 @@
 import React, { Component } from 'react';
+import { Mongo } from 'meteor/mongo'
+import $ from 'jquery'
+import { ApiCustomers } from '../../../../imports/api/customers'
 import Registration from './Registration'
+import CustomersList from './CustomersList'
 import './styles.css'
 
 export default class Customers extends Component {
-    addUserHandler (e) {
-        e.preventDefault()
-        let _user = {
-            name: e.target[0].value,
-            userName: e.target[1].value,
-            email: e.target[2].value,
-            birthDate: e.target[3].value,
-            phone: e.target[4].value,
-            password: e.target[5].value
-        }
+  addUserHandler (e) {
+    e.preventDefault()
+    let _id = new Mongo.ObjectID()
+    let _user = {
+      _id,
+      name: e.target[0].value,
+      userName: e.target[1].value,
+      email: e.target[2].value,
+      birthDate: e.target[3].value,
+      phone: e.target[4].value,
+      password: e.target[5].value
     }
-    render() {
-        return (
-            <div>
-                <h1>Customers</h1>
-                <input className='btn btn-default' id='add_user_button' name='make-action' defaultValue='Registration' />
-                <div id='add_user_form'>
-                    <h3>Register user</h3>
-                    <Registration register={this.addUserHandler} />
-                </div>
-            </div>
-        )
-    }
+    ApiCustomers.insert(_user)
+  }
+  render() {
+    return (
+        <div>
+          <h1>Customers</h1>
+          <input type='button' className='btn btn-default' id='add_user_button' className='make-action' data-target='add_user_form' value='Registration' />
+          <input type='button' className='btn btn-default' id='show_users_button' className='make-action' data-target='add_user_form' value='All users' />
+          <div id='add_user_form' style={{display: 'none'}}>
+            <h3>Register user</h3>
+            <Registration register={this.addUserHandler} />
+          </div>
+          <div id='users_list'>
+            <h3>User`s list</h3>
+            <CustomersList />
+          </div>
+        </div>
+    )
+  }
 }
+
+window.addEventListener('load', () => {
+  [...document.getElementsByClassName('make-action')].forEach((button) => {
+    button.addEventListener('click', (e) => {
+      let _target = $('#' + e.target.id).attr('data-target')
+      $('#' + _target).toggle()
+    })
+  })
+})
---
+++ client/components/Content/Customers/CustomersList/index.js
@@ -0,0 +1,16 @@
+/**
+ * Created by watcher on 10/5/16.
+ */
+import React from 'react'
+
+class CustomersList extends React.Component {
+    render () {
+        return (
+            <div>
+                List of Customers
+            </div>
+        )
+    }
+}
+
+export default CustomersList
