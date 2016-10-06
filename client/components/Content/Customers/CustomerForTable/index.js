1b3a6a87f093af0c487826433b13dae1e31df6fd
watcher <paradoxkb@gmail.com>
2016-10-06 11:13:18 +0300

    add delete`s function

--- client/components/Content/Customers/CustomerForTable/index.js
+++ client/components/Content/Customers/CustomerForTable/index.js
@@ -2,14 +2,17 @@
  * Created by watcher on 10/5/16.
  */
 import React from 'react'
+import { Link } from 'react-router'
 import './style.css'
 
 class CustomerForTable extends React.Component {
     render () {
         let { _id, name, userName, email, birthDate, role } = this.props.customer_data
+        let _href = '/customer/' + _id
         return (
-            <tr className='tr-href' onClick={() => {this.props.routerToCustomer(_id._str)}}>
-                <td>{name}</td>
+            <tr className='tr-href'>
+                <td><input type='checkbox' id={_id} name='checkbox-for-delete' onChange={this.props.handlerDeleteCustomer} /></td>
+                <td><Link to={_href}>{name}</Link></td>
                 <td>{userName}</td>
                 <td>{email}</td>
                 <td>{birthDate}</td>
--- client/components/Content/Customers/CustomersList/index.js
+++ client/components/Content/Customers/CustomersList/index.js
@@ -5,34 +5,61 @@ import { Meteor } from 'meteor/meteor'
 import { createContainer } from 'meteor/react-meteor-data'
 import { ApiCustomers } from '../../../../../imports/api/customers'
 import React from 'react'
+import $ from 'jquery'
 import CustomerForTable from '../CustomerForTable'
 import Pagination from '../Pagination'
 
 class CustomersList extends React.Component {
-    constructor(props, context) {
-        super(props, context)
+    constructor(props) {
+        super(props)
         this.state = {
             customers: [],
             currentPage: 1,
             elemsOnPage: 3,
-            maxPage: 0
+            maxPage: 0,
+            stateForRemove: []            
         }
         this.handlerPagination = this.handlerPagination.bind(this)
-        this.routerToCustomer = this.routerToCustomer.bind(this)
-        context.router
+        this.handlerDeleteCustomer = this.handlerDeleteCustomer.bind(this)
     }
     componentWillReceiveProps(nextProps) {
         let _customers = nextProps.customers
         let maxPage = Math.ceil(_customers.length / this.state.elemsOnPage)
         this.setState({customers: _customers, maxPage})
     }
+    shouldComponentUpdate (nextProps, nextState) {
+        let _check = (nextState.stateForRemove.length > 0 ) ? 0 : 1
+        return _check
+    }
     handlerPagination (num) {
         this.setState({currentPage: num})
     }
-    routerToCustomer (id) {
-        this.context.router.push(`/customers/${id}`)
+    handlerDeleteCustomer (e) {
+        let arrForRemove = this.state.stateForRemove,
+            { id, name } = e.target
+        switch (name) {
+            case 'checkbox-for-delete':
+                if($(e.target).is(':checked')){
+                    arrForRemove.push(id)
+                } else {
+                    arrForRemove = arrForRemove.filter(elem => {
+                        if (elem != id) {
+                            return elem
+                        }
+                    })
+                }
+                this.setState({stateForRemove: arrForRemove})
+                break
+            case 'remover-users':
+                arrForRemove.map(elem => {
+                    ApiCustomers.remove({_id: new Mongo.ObjectID(elem)})
+                })
+                this.setState({stateForRemove: []})                
+                break
+            default: break
+        }
     }
-    render () {
+    render () {        
         let currentNums = this.state.currentPage * this.state.elemsOnPage
         let _customers = this.state.customers.slice(currentNums - this.state.elemsOnPage, currentNums)
         return (
@@ -40,31 +67,29 @@ class CustomersList extends React.Component {
                 <table className='table table-hover'>
                     <thead>
                         <tr>
+                            <th>#</th>
                             <th>User login</th>
                             <th>User name</th>
                             <th>User email</th>
-                            <th>User Birth date</th>
+                            <th>User BirthDate</th>
                             <th>User role</th>
                         </tr>
                     </thead>
                     <tbody>
                         {_customers.map(customer => {
                             return (
-                                <CustomerForTable customer_data={customer} routerToCustomer={this.routerToCustomer} key={Math.random()} />
+                                <CustomerForTable customer_data={customer} handlerDeleteCustomer={this.handlerDeleteCustomer} key={Math.random()} />
                             )
                         })}
                     </tbody>
                 </table>
+                <input type='button' className='btn btn-danger' name='remover-users' value='Delete users' onClick={this.handlerDeleteCustomer} />
                 {(this.state.maxPage > 1) ? <div className='text-center'>
                     <Pagination num={this.state.maxPage} handlerPagination={this.handlerPagination} key={Math.random()} />
-                </div> : ''}
+                </div> : ''}                
             </div>
         )
     }
-}
-
-CustomersList.contextTypes = {
-    router: React.PropTypes.object.isRequired
 }
 
 export default createContainer(() => {
