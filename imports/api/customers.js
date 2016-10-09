3d2ed5b9cf8b9c9a341d159ebb01f8ade13ed1bd
watcher <paradoxkb@gmail.com>
2016-10-09 18:24:57 +0300

    without schema

--- imports/api/customers.js
+++ imports/api/customers.js
@@ -2,7 +2,7 @@ import { Mongo } from 'meteor/mongo';
 import Customer from './schemas/Customer'
 //create db table - customers
 export const ApiCustomers = new Mongo.Collection('customers');
-ApiCustomers.schema = new SimpleSchema(Customer)
+//ApiCustomers.schema = new SimpleSchema(Customer)
 
 //for return our Customers data
 if (Meteor.isServer) {
