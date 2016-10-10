d733cd6b109d2ea54ce0bbd6a6c4fdc0e7e00f8e
watcher <paradoxkb@gmail.com>
2016-10-10 08:52:45 +0300

    some edits

--- client/components/Content/Registration/index.js
+++ client/components/Content/Registration/index.js
@@ -7,25 +7,11 @@ import { createContainer } from 'meteor/react-meteor-data'
 import { ApiCustomers } from '../../../../imports/api/customers'
 import { imgToBase64 } from '../../../helpers/handlerImages'
 
-function toDataUrl(url, callback) {
-    console.log(url)
-    let file = url
-    let reader = new FileReader()
-    if(file) {
-        reader.readAsDataURL(file)
-    }
-    reader.onloadend = () => {
-        console.log(reader.result)
-    }
-        
-}
-
 class Registration extends React.Component {
     addUserHandler (e) {
         e.preventDefault()
         if (e.target[6].value != e.target[7].value){
             alert('Введенные пароли не совпадают');
-            e.target.reset()
             return false
         }
         let _id = new Mongo.ObjectID(),
@@ -34,8 +20,8 @@ class Registration extends React.Component {
             _images
         let fileId = _target[8].files[0],
             fileLicense = _target[9].files[0]
-        if(fileId.size > 1100000 || fileLicense.size > 1100000) {
-            alert('Please upload image less than 1mb')
+        if(fileId.size > 11000000 || fileLicense.size > 11000000) {
+            alert('Please upload image less than 10mb')
             e.preventDefault()
             return false
         }
@@ -63,35 +49,6 @@ class Registration extends React.Component {
                 _target.reset()
             })
         })
-        /*
-            reader = new FileReader()
-        reader.readAsDataURL(fileId)
-        reader.onloadend = () => {
-            imgId = reader.result
-            reader = new FileReader()
-            reader.readAsDataURL(fileLicense)
-            reader.onloadend = () => {
-                imgLicense = reader.result
-                _images = {
-                    imgId,
-                    imgLicense
-                }
-                _user = {
-                    _id,
-                    name: _target[0].value,
-                    userName: _target[1].value,
-                    email: _target[2].value,
-                    birthDate: _target[3].value,
-                    phone: _target[4].value,
-                    address: _target[5].value,
-                    password: _target[6].value,
-                    role: 'customer',
-                    _images
-                }
-                ApiCustomers.insert(_user)
-                _target.reset()
-            }
-        }*/
     }
     render () {
         return (
@@ -118,19 +75,19 @@ class Registration extends React.Component {
                 <div className='form-group'>
                     <label htmlFor='birth_date' className='control-label col-xs-2'>Birth Date</label>
                     <div className='col-xs-10'>
-                        <input type='date' id='birth_date' className='form-control' required />
+                        <input type='date' id='birth_date' className='form-control' />
                     </div>
                 </div><br />
                 <div className='form-group'>
                     <label htmlFor='phone_number' className='control-label col-xs-2'>Phone Number</label>
                     <div className='col-xs-10'>
-                        <input type='text' id='phone_number' className='form-control' required />
+                        <input type='text' id='phone_number' className='form-control' />
                     </div>
                 </div><br />
                 <div className='form-group'>
                     <label htmlFor='address' className='control-label col-xs-2'>Address</label>
                     <div className='col-xs-10'>
-                        <input type='text' id='address' className='form-control' required />
+                        <input type='text' id='address' className='form-control' />
                     </div>
                 </div><br />
                 <div className='form-group'>
@@ -171,3 +128,17 @@ export default Registration
         images: Images.find({}).fetch()
     }
 }, Registration)*/
+
+/*function toDataUrl(url, callback) {
+ console.log(url)
+ let file = url
+ let reader = new FileReader()
+ if(file) {
+ reader.readAsDataURL(file)
+ }
+ reader.onloadend = () => {
+ console.log(reader.result)
+ }
+
+ }
+*/
